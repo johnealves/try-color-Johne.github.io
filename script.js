99791401f65096f4df595c93c472868e372b2e07
Johne Alves <johne_af@hotmail.com>
2020-11-16 13:52:19 -0300

    adiciona modelo rgb

--- script.js
+++ script.js
@@ -2,25 +2,43 @@ const bolas = document.getElementsByClassName("ball");
 const textoCor = document.getElementById('rgb-color');
 const textoResposta = document.getElementById('answer')
 const botaoReiniciar = document.getElementById('reset-game');
+const tomVermelho = document.getElementById('corRed')
 const cores = [];
+const pontos = document.getElementById('score')
+let numPontos = 0
 
-function numeroAleatorio() {
+function corRed() {
+    const red = Math.floor(Math.random() * 256)
+    return red
+}
+function corGreen() {
+    const green = Math.floor(Math.random() * 256)
+    return green
+}
+function corBlue() {
+    const blue = Math.floor(Math.random() * 256)
+    return blue
+}
+
+/*function numeroAleatorio() {
     const numero = Math.floor(Math.random() * 256);
     return numero;
-};
+};*/
 
 function corAleatoria() {
-    const rgb = `rgb(${numeroAleatorio()}, ${numeroAleatorio()}, ${numeroAleatorio()})`;
+    const rgb = `rgb(${corRed()}, ${corGreen()}, ${corBlue()})`;
     return rgb;
 }
 function cliqueNaBola(event) {
     const corSelecionada = event.target.style.backgroundColor;
     if (corSelecionada === textoCor.textContent) {
         textoResposta.textContent = 'Acertou!'
+        
     } else {
         textoResposta.textContent = 'Errou! tente novamente.'
     }
 }
+
 
 for (let bola = 0; bola < bolas.length; bola++) {
     const cor = corAleatoria()
@@ -34,6 +52,8 @@ function adicionaCorAleatoria() {
 }
 
 adicionaCorAleatoria()
+
+
 
 function reiniciaJogo() {
     window.location.reload();
